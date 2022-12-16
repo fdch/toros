4908b5c9a3dd01f10aeee6b691e2dcf683ca0d37
Fede Camara Halac <camarafede@gmail.com>
2022-12-16 19:55:37 +0100

    fix proxy and server

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.get("/api", (req, res) => {
 	res.json({ message: "Hi there" });
