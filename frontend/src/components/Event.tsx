09069a6ae1053d95fc46fae5e4f61add0e7511cd
Fede Camara Halac <camarafede@gmail.com>
2023-01-13 20:26:07 +0100

    final adjustments

--- frontend/src/components/Event.tsx
+++ frontend/src/components/Event.tsx
@@ -3,9 +3,22 @@ import Link from 'next/link'
 import Image from 'next/image'
 import Typography from '@mui/material/Typography'
 import Card from '@mui/material/Card'
+import Grid from '@mui/material/Grid'
 import CardActions from '@mui/material/CardActions'
 import CardMedia from '@mui/material/CardMedia'
 import CardContent from '@mui/material/CardContent'
+import { styled } from '@mui/material/styles';
+
+
+const Item = styled(Card)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(0.2),
+  backgroundColor: "rgba(0,0,0,0.2)",
+  color: "rgba(255,255,255,1)",
+  elevation: 10,
+  textAlign: 'center',
+}));
+
 
 export type EventType = {
   url: string
@@ -14,37 +27,84 @@ export type EventType = {
   title: string
   employer: string
   date: Date
-  image: string[]
+  images: string[]
   audio: string[]
-  video: string[]
+  videos: string[]
 
 }
 
-export default function Event(event:EventType) {
+export default function Event(
+  { event } : { event: EventType }
+) {
+  const sliceURL = (url: string) => url.length>=30?url.slice(0,30)+"...":url
+  
+  const date = new Date(event.date)
+
   return (
-    <Card>
+    <Item sx={{margin:1.5}}>
+
       <CardContent>
-      <Typography>
-       {event.title}
-      </Typography>
-      <Typography>
-      {event.institution}
-      </Typography>
-      <Typography>
-      {event.date.toString()}
-      </Typography>
-      <Typography>
-      {event.employer}
-      </Typography>
-      <Typography>
-      {event.location}
-      </Typography>
+
+        <Grid container spacing={2}>
+          <Grid item>
+          <Typography sx={{ fontSize: 13 }}>
+          {event.location}
+          </Typography>
+          </Grid>
+          <Grid item>
+          <Typography sx={{ fontSize: 13 }}>
+            {date.toDateString()}
+          </Typography>
+          </Grid>
+          <Grid item>
+          <Typography sx={{ fontSize: 13 }}>
+            {date.toLocaleTimeString()}
+          </Typography>
+          </Grid>
+        </Grid>
+        
+        <Typography variant="h5" component="div">
+         {event.title}
+        </Typography>
+
+        { !event.employer ? null :
+          <Typography>
+           {event.employer}
+          </Typography>
+        }
+          <Typography sx={{ fontSize: 13 }}>
+            {event.institution}
+          </Typography>
+             
+      
+      <Grid container>
+      { !event.url ? null :
+       <Grid item>
+        <Link href={event.url}>
+        {
+        `(web) ${sliceURL(event.url)}`
+        }
+        </Link>
+       </Grid>
+      }
+      </Grid>
+      <Grid container>
+      { !event.videos ? null :
+       <Grid item>
+
+        {
+          event.videos.map((url: string, index:number) =>
+            <Link key={url} href={url}>
+              {
+              `(video) ${sliceURL(url)}`
+              }
+            </Link>
+           )  
+        }
+       </Grid>
+      }
+      </Grid>
       </CardContent>
-      <CardMedia>
-      {event.url?<Link href={event.url}>{event.url}</Link>:null}
-      </CardMedia>
-      <CardActions>
-      </CardActions>
-    </Card>
+    </Item>
   )
 }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import Grid from '@mui/material/Grid'
 import { useEffect, useState } from 'react'
 import firebaseConfig from '../config/firebase.config'
 import { getDatabase, get, ref, child } from 'firebase/database'
@@ -9,12 +10,35 @@ import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import Event, { EventType } from '../components/Event'
 
+type Member = {
+  name: string
+  url: string
+}
+
 export default function Home() {
   const app = initializeApp(firebaseConfig)
-  //const analytics = getAnalytics(app)
+  useEffect( () => {
+    const analytics = getAnalytics(app)
+  }, [app])
   const dbRef = ref(getDatabase());
   const [description, setDescription] = useState<string>('');
-  const [events, setEvents] = useState<EventType[] | undefined>();
+  const [members, setMembers] = useState<Member[] | undefined>();
+  const [eventsPast, setEventsPast] = useState<EventType[] | undefined>();
+  const [eventsNext, setEventsNext] = useState<EventType[] | undefined>();
+
+  useEffect(() => {
+    get(child(dbRef, '/members')).then((snapshot) => {
+      if (snapshot.exists()) {
+        setMembers(snapshot.val())
+      } else {
+        console.log("No data")
+      }
+    }).catch((error) => {
+      console.error(error)
+    })
+  }, []);
+
+
   useEffect(() => {
     get(child(dbRef, '/description')).then((snapshot) => {
       if (snapshot.exists()) {
@@ -26,6 +50,13 @@ export default function Home() {
       console.error(error)
     })
   }, []);
+
+  const setEvents = (events: EventType[]) => {
+    const today = new Date
+    setEventsNext(events.filter((e:EventType) => new Date(e.date) > today))
+    setEventsPast(events.filter((e:EventType) => new Date(e.date) <= today))
+  }
+
   useEffect(() => {
     get(child(dbRef, '/events')).then((snapshot) => {
       if (snapshot.exists()) {
@@ -37,11 +68,12 @@ export default function Home() {
       console.error(error)
     })
   }, []);
+  
   return (
     <>
       <Head>
         <title>toros</title>
-        <meta name="description" content="experimental music duo" />
+        <meta name="description" content={description} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="" />
       </Head>
@@ -58,45 +90,65 @@ export default function Home() {
       />
     </div>
     <main className={styles.main}>
+   
 
        <h1>toros</h1>
+       <h2>an experimental music duo</h2>
+
         <div className={styles.center}>
-         <p>{description ? description : "Loading..."}</p>
+          { description
+          ?
+           <p>{description}</p>
+          :
+           <p>Loading description ...</p>
+          }
         </div>
-        
-       <div className={styles.grid}>
-        { events 
-        ? 
-          <div> 
-          <h2>Past Events</h2>
-          <table className={styles.card}>
-            <thead>
-              <tr>
-                <td>Title</td>
-                <td>Institution</td>
-                <td>Date</td>
-                <td>Employer</td>
-                <td>Location</td>
-                <td>Url</td>
-              </tr>
-            </thead>
-            <tbody>
-              {events.map((event: EventType, index: number) =>
-               <tr key={`${index}-${event.title}`}>
-                <td>{event.title}</td>
-                <td>{event.institution}</td>
-                <td>{event.date.toString()}</td>
-                <td>{event.employer}</td>
-                <td>{event.location}</td>
-                <td>{event.url?<Link href={event.url}>{event.url}</Link>:null}</td>
-               </tr>
+
+        { eventsNext 
+        ? eventsNext.length == 0 ? null :
+          <>
+          <h2>Next Events</h2>
+          <div className={styles.grid}>
+              {eventsNext.map((event: EventType,index: number) =>
+               <Event key={`${index}-${event.title}`} event={event}/>
               ).reverse()}
-            </tbody>
-          </table>
           </div>
+          </>
         : 
-          "Loading..." }
-       </div>
+          <p>Loading next events...</p>
+        }
+
+        { eventsPast  
+        ? eventsPast.length == 0 ? null :
+          <>
+          <h2>Past Events</h2>
+          <div className={styles.grid}>
+              {eventsPast.map((event: EventType,index: number) =>
+               <Event key={`${index}-${event.title}`} event={event}/>
+              ).reverse()}
+          </div>
+          </>
+        : 
+          <p>Loading past events...</p>
+        }
+        
+        { members
+        ? 
+         <>
+         <h2>About Us</h2>
+         <div className={styles.grid}>
+          {members.map((member: Member) => 
+            <>
+              <h3>{member.name}</h3>
+              <Link href={member.url}>{member.url}</Link>
+            </>
+          )}
+         </div>
+         </>
+        : 
+          <p>Loading members... </p>
+        }
+
      </main> 
   </>
   )
